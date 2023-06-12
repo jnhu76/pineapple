588365f65f1e80d4e6ae13d4339b577c1443b38b
JnHu <me@hoooo.org>
2023-06-12 20:54:59 +0800

    add readfile support

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
+#include <string_view>
 #include "pineapple.h"
 
 int main(int argc, char** argv)
 {
 
-    auto s = std::string("$a = \"pen pineapple\n\rapple pen.\"\nprint($a)\r$a = \"another test 1123\" $a = \"overwrite test\n\rwith new line\" print( $a )");
+    // auto s = std::string("$a = \"pen pineapple\n\rapple pen.\"\nprint($a)\r$a = \"another test 1123\" $a = \"overwrite test\n\rwith new line\" print( $a )");
 
-    // if (argc <= 1) {
-    //     std::cerr << "File not found.\n";
-    //     exit(1);
-    // }
+    if (argc <= 1) {
+        std::cerr << "File not found.\n";
+        exit(1);
+    }
 
-    // std::string file = argv[1];
+    std::string_view file = argv[1];
 
-    // auto data = read_file(file);
+    auto data = read_file(file);
 
-    // Interpreter i(data);
-
-    Interpreter i(s);
+    Interpreter i(data);
+    // Interpreter i(s);
     i.execute();
 
     return 0;
